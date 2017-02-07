d82adc84481b78e8e3e7c73d3896c5217181c8bc
snakierten96 <neil.lunn1@gmail.com>
2017-02-08 08:01:07 +1100

    Style elements

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -14,23 +14,6 @@ export class CategoryComponent implements OnInit {
 
   list: IItem[] = [];
 
-  /*
-  list: IItem[] = [
-    { pos: 1, title: 'One' },
-    { pos: 2, title: 'Two' },
-    { pos: 3, title: 'Three' },
-    { pos: 4, title: 'Four' },
-    { pos: 5, title: 'Five' },
-    { pos: 6, title: 'Six' },
-    { pos: 7, title: 'Seven' },
-    { pos: 8, title: 'Eight' },
-    { pos: 9, title: 'Nine' },
-    { pos: 10, title: 'Ten' },
-    { pos: 11, title: 'Eleven' },
-    { pos: 12, title: 'Twelve' }
-  ];
-  */
-
   private _subscriptions: Subscription[] = [];
 
   constructor(private route: ActivatedRoute) { }
